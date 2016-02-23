93508ff4b20625d8691df1bcd06dbd2e04eae30c
Romain <test@test.com>
2016-02-23 16:35:58 +0100

    Q6

--- js/pokedex.js
+++ js/pokedex.js
@@ -4,4 +4,21 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
-var pokeApiUrl = "http://pokeapi.co/"
+var pokeApiUrl = "http://pokeapi.co/";
+
+pokeApp.controller('pokemonSearchController', ['$scope', function($scope) {
+  var _id = 0;
+  var _list = ['pikachu', 'Miaousse', 'Carapuce', 'Salameche'];
+  var _name = '';
+  $scope.pokemon = {
+    id: function(newId) {
+     return arguments.length ? (_id = newId) : _id;
+   },
+   list: function(){
+     return _list;
+   },
+   name: function(newName){
+     return arguments.length ? (_name = newName) : _name;
+   }
+  };
+}]);
